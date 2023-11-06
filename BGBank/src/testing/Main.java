eda4627788775dd8ac4213ef32c2c429bba96308
batgar37 <baptiste.garrido@yahoo.fr>
2023-11-06 15:06:10 +0100

    adaptation of the table of accounts

--- BGBank/src/testing/Main.java
+++ BGBank/src/testing/Main.java
@@ -1,6 +1,8 @@
 package testing;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import components.Account;
 import components.Client;
@@ -15,12 +17,16 @@ public class Main {
 		ArrayList<Client> arrayClient = generateClients(10);
 		ArrayList<Account> arrayAccount = generateAccounts(arrayClient);
 
+		HashMap<Integer, Account> hashAccount = generateHashAccount(arrayAccount);
+
 		displayClients(arrayClient);
 		displayAccounts(arrayAccount);
+		displayHash(hashAccount);
+
 	}
 
 	private static ArrayList<Client> generateClients(int toCreate) {
-		ArrayList<Client> arrayClient = new ArrayList<Client>();
+		ArrayList<Client> arrayClient = new ArrayList<>();
 		for (int i = 0; i < toCreate; i++) {
 			arrayClient.add(new Client("name" + i, "firstname" + i));
 		}
@@ -33,7 +39,7 @@ public class Main {
 	}
 
 	private static ArrayList<Account> generateAccounts(ArrayList<Client> arrayClient) {
-		ArrayList<Account> arrayAccount = new ArrayList<Account>();
+		ArrayList<Account> arrayAccount = new ArrayList<>();
 		for (Client client : arrayClient) {
 			arrayAccount.add(new SavingsAccount("Saving account client" + client.getClientNumber(), client));
 			arrayAccount.add(new CurrentAccount("Current account client" + client.getClientNumber(), client));
@@ -43,5 +49,20 @@ public class Main {
 
 	private static void displayAccounts(ArrayList<Account> arrayAccount) {
 		arrayAccount.forEach(System.out::println);
+	}
+
+	// 1.3.1 Adaptation of the table of accounts
+	private static HashMap<Integer, Account> generateHashAccount(ArrayList<Account> arrayAccount) {
+		HashMap<Integer, Account> hashtable = new HashMap<>();
+
+		for (Account account : arrayAccount) {
+			hashtable.put(account.getAccountNumber(), account);
+		}
+
+		return hashtable;
+	}
+
+	private static void displayHash(HashMap<Integer, Account> hashAccount) {
+		hashAccount.entrySet().stream().sorted(Map.Entry.comparingByValue());
 	}
 }
